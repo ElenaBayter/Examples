4675c96bd6120701074d0601ad6263501f962b96
elena tarasova <nafanichka.ev@gmail.com>
2022-09-13 18:38:00 -0400

    add ConditionIfEsle

---
+++ Example005_ContitionIfElse/Program.cs
@@ -0,0 +1,12 @@
+﻿Console.Write("TextWriter username: ");
+string username = Console.ReadLine();
+
+if (username.ToLower() == "masha")
+{
+    Console.WriteLine("Hurray, its MASHA");
+}
+else
+{
+    Console.Write("Hello, ");
+    Console.WriteLine(username);
+}
